d5a21ea02561fb3338a541a8ba1ca1a31a8b5a3c
Aleksandr <nastyavin86@br.ru>
2022-01-17 20:03:30 +0300

    file change brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
+
+import prompt
+from brain_games import cli
+
+
 def main():
     print("Welcom to the Brain Games!")
+    name =cli.welcome_user()
+    print('Hello, name!')
 
 
 if __name__ == '__main__':
