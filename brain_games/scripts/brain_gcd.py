3cf970346e9ed2510e92ef244a717e94212da48d
Aleksandr <nastyavin86@br.ru>
2022-01-22 20:36:41 +0300

    update file

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -9,15 +9,15 @@ from brain_games.games import general_conditions
 name = general_conditions.greetings()
 
 
-def calculate():
+def greatest_common_divisor():
     """The user must calculate and enter the greatest common\
         divisor of these numbers."""
     print('Find the greatest common divisor of given numbers.')
     count = 0
     while count != 3:
-        num1 = randint(1, 200)
-        num2 = randint(1, 200)
-        result = gcd(num1, num2)
+        num1 = randint(1, 20)
+        num2 = randint(1, 20)
+        result = str(gcd(num1, num2))
         print('Question:', num1, num2)
         answer = prompt.string('Your answer: ')
         check = general_conditions.conclusion(answer, result, name, count)
@@ -26,4 +26,4 @@ def calculate():
 
 
 if __name__ == '__main__':
-    calculate()
+     greatest_common_divisor()
