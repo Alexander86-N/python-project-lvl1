621dd6fafe7e184055033c7dd4c6b0821c6dde19
Aleksandr <nastyavin86@br.ru>
2022-01-22 18:43:39 +0300

    add general_conditions.py

---
+++ brain_games/games/general_conditions.py
@@ -0,0 +1,25 @@
+import prompt
+from random import randint
+
+
+def greetings():
+    """Greeting and introduction to the player."""
+    print("Welcom to the Brain Games!")
+    name = prompt.string('May I have your name? ')
+    print('Hello, {}!'.format(name))
+    return name
+
+
+
+def derivation_of_conditions(answer, result, name, count):
+    """Returns the result of the condition."""
+    check = 0
+    if answer == result:
+        check += 1
+        print('Correct!')
+    else:
+        print("'{}' is wrong answer ;(.\
+Correct answer was '{}'.".format(answer, result))
+        print("Let's try again, {}!".format(name))
+    return check
+
