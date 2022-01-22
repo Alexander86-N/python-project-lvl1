9f6e7b702c9456ac746a3d4b67868135fbae8cdf
Aleksandr <nastyavin86@br.ru>
2022-01-22 22:22:13 +0300

    add brain_progression.py

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import prompt
+from random import randint
+from brain_games.games import general_conditions
+
+
+name = general_conditions.greetings()
+
+
+def greatest_common_divisor():
+    """Determining the missing number in an arithmetic progression"""
+    print('What number is missing in the progression?')
+    count = 0
+    while count != 3:
+        num1 = randint(1, 10)
+        num2 = randint(1, 10)
+        idx = randint(0, 9)
+        my_list = []
+        my-list.append(num1)
+        for i in range(9):
+            num = num1 + num2
+            my_list.append(num)
+            num1 = num
+        result = str(my_list[idx])
+        my_list[ind] = '..'
+        print('Question:', ' '.join(my_list))
+        answer = prompt.string('Your answer: ')
+        check = general_conditions.conclusion(answer, result, name, count)
+        count += check
+    print('Congratulations, {}!'.format(name))
+
+
+if __name__ == '__main__':
+    greatest_common_divisor()
