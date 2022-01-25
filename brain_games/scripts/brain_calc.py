1eb87e6064ba8e97b096b6d4d9b7bc2d58d8e4ad
Aleksandr <nastyavin86@br.ru>
2022-01-25 20:56:04 +0300

    update brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,28 +1,14 @@
 #!/usr/bin/env python
 
-import prompt
-from random import randint, choice
-from brain_games.games import general_conditions
+from brain_games import general_logic
+from brain_games.games import calculator
 
-
-name = general_conditions.greetings()
 
 
 def calculate():
     """The user is shown a random mathematical expression that needs \
        to be calculatedand the correct answer written down."""
-    print('What is the result of the expression?')
-    count = 0
-    while count != 3:
-        num1 = str(randint(1, 25))
-        num2 = str(randint(1, 25))
-        sing = choice(['+', '-', '*'])
-        result = str(eval(num1 + sing + num2))
-        print('Question:', num1, sing, num2)
-        answer = prompt.string('Your answer: ')
-        check = general_conditions.conclusion(answer, result, name, count)
-        count += check
-    print('Congratulations, {}!'.format(name))
+    general_logic.common_engine(calculator)
 
 
 if __name__ == '__main__':
