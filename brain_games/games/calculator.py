f1263df3d3951810f18da5932e83d3833ab32b75
Aleksandr <nastyavin86@br.ru>
2022-01-25 20:45:15 +0300

    add calculator.py

---
+++ brain_games/games/calculator.py
@@ -0,0 +1,15 @@
+from random import randint, choice
+
+GAME_DESCRIPTION = 'What is the result of the expression?.'
+
+
+def definition_of_arguments():
+    num1 = str(randint(1, 25))
+    num2 = str(randint(1, 25))
+    sing = choice(['+', '-', '*'])
+    return '{} {} {}'.format(num1, sing, num2)
+
+
+def correct_answer(number):
+    result = str(eval(number)))
+    return result
