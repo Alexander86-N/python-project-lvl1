817c28924d7cc952e497e29bb676c090f4423ad1
Aleksandr <nastyavin86@br.ru>
2022-01-25 18:21:27 +0300

    update general_logic.py

--- brain_games/general_logic.py
+++ brain_games/general_logic.py
@@ -6,16 +6,20 @@ def common_engine():
     print("Welcome to the Brain Games!")
     name = prompt.string('May I have your name? ')
     print('Hello, {}!'.format(name))
-
-
-
-
-    check = 0
-    if answer == result:
-        check += 1
-        print('Correct!')
-    else:
-        print("'{}' is wrong answer ;(.\
+    print(GAME_DESCRIPTION)
+    NUMBER_OF_ROUNDS = 3
+    while check != NUMBER_OF_ROUNDS:
+        print('Question:', definition_of_arguments())
+        answer = prompt.string('Your answer: ')
+        result = correct_answer()
+        check = 0
+        if answer == result:
+            check += 1
+            print('Correct!')
+        else:
+            print("'{}' is wrong answer ;(.\
 Correct answer was '{}'.".format(answer, result))
-        print("Let's try again, {}!".format(name))
-    return check
+            print("Let's try again, {}!".format(name))
+            break
+        return check
+     print('Congratulations, {}!'.format(name))
