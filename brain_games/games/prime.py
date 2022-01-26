d07b9c8c165403152250c8723a157d5b1db12f09
Aleksandr <nastyavin86@br.ru>
2022-01-26 20:28:23 +0300

    update prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -13,7 +13,7 @@ def definition_of_arguments():
 def correct_answer(number):
     """Primality test."""
     result = ''
-    for i in range(1, number):
+    for i in range(2, number):
         if number % i == 0:
             result = 'no'
             break
