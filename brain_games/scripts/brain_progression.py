2e92686771f6d09525e07b19f2e25f8c3cdd9b71
Aleksandr <nastyavin86@br.ru>
2022-01-26 17:42:25 +0300

    fix brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,35 +1,12 @@
 #!/usr/bin/env python
 
-import prompt
-from random import randint
-from brain_games.games import general_conditions
-
-
-name = general_conditions.greetings()
+from brain_games import general_logic
+from brain_games.games import ar_progression
 
 
 def work_with_progression():
     """Determining the missing number in an arithmetic progression"""
-    print('What number is missing in the progression?')
-    count = 0
-    while count != 3:
-        num1 = randint(1, 10)
-        num2 = randint(1, 10)
-        num3 = randint(6, 10)
-        idx = randint(0, num3)
-        my_list = []
-        my_list.append(str(num1))
-        for i in range(num3):
-            num = num1 + num2
-            my_list.append(str(num))
-            num1 = num
-        result = str(my_list[idx])
-        my_list[idx] = '..'
-        print('Question:', ' '.join(my_list))
-        answer = prompt.string('Your answer: ')
-        check = general_conditions.conclusion(answer, result, name, count)
-        count += check
-    print('Congratulations, {}!'.format(name))
+    general_logic.common_engine(ar_progression)
 
 
 if __name__ == '__main__':
