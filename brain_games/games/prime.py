6cc1db3029dba03e29d04d4542bf16bb2daa1388
Aleksandr <nastyavin86@br.ru>
2022-01-26 18:19:34 +0300

    update prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -6,7 +6,7 @@ GAME_DESCRIPTION = 'Answer "yes" if given number is prime.\
 
 def definition_of_arguments():
     """Picking a random number."""
-    num = randint(2, 15)
+    num = randint(2, 25)
     return num
 
 
@@ -15,6 +15,7 @@ def correct_answer(number):
     for i in range(2, number // 2):
         if number % i == 0:
             result = 'no'
+            break
         else:
             result = 'yes'
     return result
