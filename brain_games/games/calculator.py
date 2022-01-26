abe6223e8f3f2c6b80ff0f14c4fca82ac81acf6d
Aleksandr <nastyavin86@br.ru>
2022-01-26 18:32:08 +0300

    update calculator.py

--- brain_games/games/calculator.py
+++ brain_games/games/calculator.py
@@ -4,6 +4,7 @@ GAME_DESCRIPTION = 'What is the result of the expression?.'
 
 
 def definition_of_arguments():
+    """Selection of random numbers and operations."""
     num1 = str(randint(1, 25))
     num2 = str(randint(1, 25))
     sing = choice(['+', '-', '*'])
@@ -11,5 +12,6 @@ def definition_of_arguments():
 
 
 def correct_answer(number):
+    """Calculates the result of a mathematical expression."""
     result = str(eval(number))
     return result
