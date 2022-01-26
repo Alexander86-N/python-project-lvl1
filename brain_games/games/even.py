5a5d418317a4a88c93852dfe8919a2e3e1f5eef3
Aleksandr <nastyavin86@br.ru>
2022-01-26 18:24:23 +0300

    update even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -4,11 +4,13 @@ GAME_DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def definition_of_arguments():
+    """Picking a random number."""
     num = randint(1, 99)
     return num
 
 
 def correct_answer(number):
+    """Checks if a number is even."""
     if number % 2 == 0:
         result = 'yes'
     else:
