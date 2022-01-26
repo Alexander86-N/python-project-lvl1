d74c2b1e993fae0184bc8547c9c891dbf575eef1
Aleksandr <nastyavin86@br.ru>
2022-01-26 18:37:33 +0300

    update gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,16 +1,18 @@
 from math import gcd
 from random import randint
 
-GAME_DESCRIPTION = 'Find the greatest common divisor of given numbers..'
+GAME_DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 
 
 def definition_of_arguments():
+    """Selection of random numbers."""
     num1 = randint(2, 50)
     num2 = randint(2, 50)
     return '{} {}'.format(num1, num2)
 
 
 def correct_answer(number):
+    """Calculates the greatest common divisor of these numbers."""
     num = number.split()
     result = str(gcd(int(num[0]), int(num[1])))
     return result
