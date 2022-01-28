6359f261097ea2e83b114c35463d845b1cde05ea
Aleksandr <nastyavin86@br.ru>
2022-01-29 00:43:38 +0300

    update progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,13 +3,13 @@ from random import randint
 GAME_DESCRIPTION = 'What number is missing in the progression?'
 
 
-def generate_progression(a1, d, length):
+def generate_progression(first_term, difference, length, replacement_index):
     """Computing members of a progression"""
     """Getting a string representation of a progression"""
     numbs = []
     for i in range(length):
-        numbs.append(str(a1 + i * d))
-    numbers[randint(1,length)] = '..'
+        numbs.append(str(first_term + i * difference))
+    numbers[replacement_index] = '..'
     return ' '.join(numbers)
  #   return numbs
 
@@ -21,13 +21,23 @@ def generate_progression(a1, d, length):
  #   return ' '.join(numbers)
 
 
-def correct_answer(number):
+def correct_answer(number, item):
     """Calculating the correct answer"""
     nums = number.split()
-    idx = nums.index('..')
-    if idx >= 2:
+    if item >= 2:
         d = int(nums[1]) - int(nums[0])
     else:
-        d = int(nums[idx + 2]) - int(nums[idx + 1])
-    result = str(int(nums[0]) + idx * d)
+        d = int(nums[item + 2]) - int(nums[item + 1])
+    result = str(int(nums[0]) + item * d)
     return result
+
+
+def generate_question_and_answer():
+     """Returns a variant of the question and the correct answer to it."""
+    a1 = randint(1, 25)
+    d = randint(1, 25)
+    length = randint(6, 10)
+    item = randint(1, length)
+    example = generate_progression(a1, d, length, item)
+    result = str(correct_answer(example, item))
+    return example, result
