f010e75836e1ccc1146762b643c1a683f474bdf2
Aleksandr <nastyavin86@br.ru>
2022-01-29 14:42:40 +0300

    update brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-from brain_games import general_logic
+from brain_games import engine
 from brain_games.games import calculator
 
 
 def calculate():
     """The user is shown a random mathematical expression that needs \
        to be calculatedand the correct answer written down."""
-    general_logic.common_engine(calculator)
+    engine.start(calculator)
 
 
 if __name__ == '__main__':
