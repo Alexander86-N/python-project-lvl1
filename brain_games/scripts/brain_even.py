9fc00f3df026d29152b384a4c7f07937788478cc
Aleksandr <nastyavin86@br.ru>
2022-01-29 12:43:44 +0300

    update brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
 
-from brain_games import general_logic
+from brain_games import engine
 from brain_games.games import even
 
 
 def check_for_even():
     """Implements the game process of mini-games Check for Even."""
-    general_logic.common_engine(even)
+    engine.start(even)
 
 
 if __name__ == '__main__':
