6ab4956a1031fcadd191f08f96c355932b628da1
Aleksandr <nastyavin86@br.ru>
2022-01-29 14:13:09 +0300

    update even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -3,7 +3,7 @@ from random import randint
 GAME_DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def correct_answer(number):
+def search_answer(number):
     """Checks if a number is even."""
     if number % 2 == 0:
         result = 'yes'
@@ -15,5 +15,5 @@ def correct_answer(number):
 def generate_question_and_answer():
      """Returns a variant of the question and the correct answer to it."""
     example = randint(1, 99)
-    result = str(correct_answer(example))
+    result = str(search_answer(example))
     return example, result
