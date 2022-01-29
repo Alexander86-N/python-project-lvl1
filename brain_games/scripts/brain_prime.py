cfa18bb259a9c8b83917695f79c85164bd7a88ed
Aleksandr <nastyavin86@br.ru>
2022-01-29 14:47:35 +0300

    update brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-from brain_games import general_logic
+from brain_games import engine
 from brain_games.games import prime
 
 
 def prime_number():
     """Asking if a number is prime"""
-    general_logic.common_engine(prime)
+    engine.start(prime)
 
 
 if __name__ == '__main__':
