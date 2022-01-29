375c1c574e59a295fae4048b0bbfabd8df6e330a
Aleksandr <nastyavin86@br.ru>
2022-01-29 14:44:43 +0300

    update brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-from brain_games import general_logic
+from brain_games import engine
 from brain_games.games import gcd
 
 
 def greatest_common_divisor():
     """The user must calculate and enter the greatest common\
         divisor of these numbers."""
-    general_logic.common_engine(gcd)
+    engine.start(gcd)
 
 
 if __name__ == '__main__':
