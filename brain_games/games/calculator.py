cae20f0dedd74ad2ce0cdba1f002aeb6a81e9223
Aleksandr <nastyavin86@br.ru>
2022-01-29 13:33:50 +0300

    update calculator.py

--- brain_games/games/calculator.py
+++ brain_games/games/calculator.py
@@ -1,21 +1,7 @@
 from random import randint, choice
 from operator import add, sub, mul
 
-GAME_DESCRIPTION = 'What is the result of the expression?.'
-
-
-#def definition_of_arguments():
-#    """Selection of random numbers and operations."""
-#    num1 = str(randint(1, 25))
-#    num2 = str(randint(1, 25))
-#    sing = choice(['+', '-', '*'])
-#    return '{} {} {}'.format(num1, sing, num2)
-
-
-#def correct_answer(number):
-#    """Calculates the result of a mathematical expression."""
-#    result = str(eval(number))
-#    return result
+GAME_DESCRIPTION = 'What is the result of the expression?'
 
 
 def calc(number1,number2,sing):
