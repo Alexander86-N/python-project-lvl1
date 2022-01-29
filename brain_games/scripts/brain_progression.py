2a8f6447b7339d642f665c62d13a1131481c0ca4
Aleksandr <nastyavin86@br.ru>
2022-01-29 14:46:21 +0300

    update brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-from brain_games import general_logic
-from brain_games.games import ar_progression
+from brain_games import engine
+from brain_games.games import progression
 
 
 def work_with_progression():
     """Determining the missing number in an arithmetic progression"""
-    general_logic.common_engine(ar_progression)
+    engine.start(progression)
 
 
 if __name__ == '__main__':
