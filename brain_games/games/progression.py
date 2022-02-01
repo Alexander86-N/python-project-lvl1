8318994e52ddd123bd727b667e47bdc35a5d6ca0
Aleksandr <nastyavin86@br.ru>
2022-02-01 19:45:11 +0300

    fix progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,25 +3,13 @@ from random import randint
 GAME_DESCRIPTION = 'What number is missing in the progression?'
 
 
-def generate_progression(first_term, difference, length, index):
+def generate_progression(first_term, difference, length):
     """Computing members of a progression"""
     """Getting a string representation of a progression"""
     numbers = []
     for i in range(length):
         numbers.append(str(first_term + i * difference))
-    numbers[index] = '..'
-    return ' '.join(numbers)
-
-
-def search_answer(number, item):
-    """Calculating the correct answer"""
-    nums = number.split()
-    if item >= 2:
-        d = int(nums[1]) - int(nums[0])
-    else:
-        d = int(nums[item + 2]) - int(nums[item + 1])
-    result = str(int(nums[0]) + item * d)
-    return result
+    return numbers
 
 
 def generate_question_and_answer():
@@ -29,7 +17,8 @@ def generate_question_and_answer():
     a1 = randint(1, 25)
     d = randint(1, 25)
     length = randint(6, 10)
-    item = randint(1, length - 1)
-    example = generate_progression(a1, d, length, item)
-    result = str(search_answer(example, item))
-    return example, result
+    index = randint(1, length - 1)
+    example = generate_progression(a1, d, length)
+    result = str(example[index])
+    example[index] = '..'
+    return ' '.join(example), result
