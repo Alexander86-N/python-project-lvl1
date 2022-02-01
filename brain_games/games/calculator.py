6a0e0f1192d679ce31efc74ec7ca20055238adba
Aleksandr <nastyavin86@br.ru>
2022-02-01 18:47:40 +0300

    fix calculator.py

--- brain_games/games/calculator.py
+++ brain_games/games/calculator.py
@@ -4,9 +4,9 @@ from operator import add, sub, mul
 GAME_DESCRIPTION = 'What is the result of the expression?'
 
 
-def calc(number1, number2, sing):
+def calc(number1, number2, operation):
     """Calculates the result of a mathematical expression."""
-    result = sing(number1, number2)
+    result = operation(number1, number2)
     return result
 
 
@@ -14,7 +14,7 @@ def generate_question_and_answer():
     """Selection of random numbers and operations."""
     num1 = randint(1, 25)
     num2 = randint(1, 25)
-    sing = choice([['+', add], ['-', sub], ['*', mul]])
+    operation = choice([['+', add], ['-', sub], ['*', mul]])
     example = '{} {} {}'.format(num1, sing[0], num2)
     result = str(calc(num1, num2, sing[1]))
     return example, result
